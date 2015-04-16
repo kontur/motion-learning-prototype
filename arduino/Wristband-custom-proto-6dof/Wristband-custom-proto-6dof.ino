4bec27523cda49963dcceac0364a501fde9a4fdc
Johannes Neumeier <hello@johannesneumeier.com>
2015-04-16 17:37:40 +0300

    a lot more work on the 6dof proto; lights responding to roll, buzzer giving heading feedback

--- arduino/Wristband-custom-proto-6dof/Wristband-custom-proto-6dof.ino
+++ arduino/Wristband-custom-proto-6dof/Wristband-custom-proto-6dof.ino
@@ -3,11 +3,16 @@
 #include <SFE_LSM9DS0.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
+
+// driver for the led pwm module
 #include <Adafruit_PWMServoDriver.h>
+
+// the 6DoF module's libraries
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
 
+#include "pitches.h"
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
@@ -21,6 +26,8 @@ const int tx = 8;
 SoftwareSerial mySerial(rx, tx);
 
 const int onBoardLedPin = 6;
+const int vibrationPin = 5;
+const int buzzerPin = 11;
 
 int data[2];
 int serialIndex = 0;
@@ -43,6 +50,16 @@ float angles[3]; // yaw pitch roll
 // Set the FreeSixIMU object
 FreeSixIMU dof = FreeSixIMU();
 
+int notes[] = {
+  NOTE_C4,
+  NOTE_D4,
+  NOTE_E4,
+  NOTE_F4,
+  NOTE_G4,
+  NOTE_A4,
+  NOTE_B4,
+};
+
 
 /*
 ///////////////////////
@@ -50,8 +67,8 @@ FreeSixIMU dof = FreeSixIMU();
 ///////////////////////
 // Comment out this section if you're using SPI
 // SDO_XM and SDO_G are both grounded, so our addresses are:
-#define LSM9DS0_XM  0x1D // Would be 0x1E if SDO_XM is LOW
-#define LSM9DS0_G   0x6B // Would be 0x6A if SDO_G is LOW
+LSM9DS0_XM  0x1D // Would be 0x1E if SDO_XM is LOW
+LSM9DS0_G   0x6B // Would be 0x6A if SDO_G is LOW
 // Create an instance of the LSM9DS0 library called `dof` the
 // parameters for this constructor are:
 // [SPI or I2C Mode declaration],[gyro I2C address],[xm I2C add.]
@@ -60,10 +77,10 @@ LSM9DS0 dof(MODE_I2C, LSM9DS0_G, LSM9DS0_XM);
 // Do you want to print calculated values or raw ADC ticks read
 // from the sensor? Comment out ONE of the two #defines below
 // to pick:
-#define PRINT_CALCULATED
-//#define PRINT_RAW
+PRINT_CALCULATED
+//PRINT_RAW
 
-#define PRINT_SPEED 500 // 500 ms between prints
+PRINT_SPEED 500 // 500 ms between prints
 */
 
 
@@ -80,7 +97,7 @@ const int led2B = 9;
 
 const int led3R = 7;
 const int led3G = 8;
-const int led3B = 9;
+const int led3B = 6;
 
 const int led4R = 4;
 const int led4G = 5;
@@ -122,11 +139,16 @@ void setup ()
 
 String test = "";
 
-float lastHue = 0.25;
-float lastSaturation = 0.75;
+float lastHue = 0;
+float lastSaturation = 0;
 float lastBrightness = 1;
 
 int rgbColor[3];
+int lastVibration = 0;
+
+int note = 0;
+int lastNote = 0;
+int lastNoteStarted = 0;
 
 /*
 RGBConverter col = RGBConverter();
@@ -134,6 +156,10 @@ RGBConverter col = RGBConverter();
 
 void loop ()
 {
+
+  // reading bluetooth
+  // =================
+
   // code for reading IN information from bluetooth
   // this will be used for "playback" mode
   // read in strings as one until the last ";", then split by colon ":"
@@ -160,6 +186,9 @@ void loop ()
   }
 
 
+  // reading sensors
+  // ===============
+
   // calculate new readings from sensor and ease them from last values
   dof.getYawPitchRoll(angles);
 
@@ -168,10 +197,10 @@ void loop ()
   float rollReading = angles[2];
 
   float heading = lastHeading * easing + (1 - easing) * headingReading;
-  //float pitch = lastPitch * easing + (1 - easing) * orientation[0];
-  //float roll = lastRoll * easing + (1 - easing) * orientation[1];
 
   // these will range from -45 to 45
+  // easing defines the portion of the previous value that is blended with
+  // the new reading, thus easing the value to be less jumpy for drastic changes
   float pitch = lastPitch * easing + (1 - easing) * pitchReading;
   float roll = lastRoll * easing + (1 - easing) * rollReading;
 
@@ -183,38 +212,93 @@ void loop ()
   mySerial.println(p);
   //Serial.println(p);
 
+
+
+  // calculate some values for feedback from current sensor readings
+
+  float newHue = map(roll, -45, 45, 0, 100.0) / 100.0;
+  float hueDifference = lastHue - newHue;
+  float headingDifference = lastHeading - heading;
+
+
+  // play notes on movement
+  // ======================
+
+  Serial.println(headingDifference);
+  Serial.println(heading);
+
+  if (abs(headingDifference) > 5) {
+    int noteIndex = map(headingDifference, -90, 90, 0, 6);
+    note = notes[noteIndex];
+    Serial.println("new note");
+    Serial.println(note);
+  }
+
+  if (lastNoteStarted == 0 || lastNoteStarted > millis() + 1000 / 3) {
+    Serial.println("play new note");
+    if (note > 0 && note != lastNote) {
+      tone(buzzerPin, note, 1000 / 3);
+      lastNote = note;
+    }
+  }
+
+
+
+
+  // coloring the leds of the device
+  // ===============================
+
+  // get rgb value from hsl
+  H2R_HSBtoRGBfloat(newHue, 1, 1, &rgbColor[0]);
+
+  // store the current color for comparison on next loop
+  lastHue = newHue;
+
+  setRGBs(rgbColor[0], rgbColor[1], rgbColor[2]);
+
   /*
-  // rgb color tests
-  setRGB(1, random(255), random(255), random(255));
-  setRGB(2, random(255), random(255), random(255));
-  setRGB(3, random(255), random(255), random(255));
-  setRGB(4, random(255), random(255), random(255));
-  */
-
-  lastHue = lastHue + 0.05;
-  if (lastHue > 1) {
-    lastHue = 0;
-  }
-
-  lastBrightness = lastBrightness + 0.1;
-  if (lastBrightness > 255) {
-    lastBrightness = 0;
-  }
-
-
-  H2R_HSBtoRGBfloat(lastHue, lastSaturation, lastBrightness, &rgbColor[0]);
-  setRGB(1, rgbColor[0], rgbColor[1], rgbColor[2]);
-  setRGB(3, rgbColor[0], rgbColor[1], rgbColor[2]);  
-  setRGB(2, 0, 0, 0);
-  setRGB(4, 0, 0, 0);
   Serial.println(rgbColor[0]);
   Serial.println(rgbColor[1]);
   Serial.println(rgbColor[2]);
+  */
 
 
-  // interval at which to send and receive from bluetooth, 12fps
-  delay(1000 / 12);
-  //delay(2000);
+  // giving vibration feedback
+  // =========================
+  // abs() does a bit weird things, so tenary abs() and * 100 in one
+  hueDifference = hueDifference < 0 ? hueDifference * -100 : hueDifference * 100;
+  // * 5 to amplify difference value => sensitiviy
+  hueDifference = constrain(hueDifference * 4, 0, 100);
+  int vibration = map(hueDifference, 0, 100, 0, 255);
+  if (vibration > 125) {
+    analogWrite(vibrationPin, vibration);
+  } else {
+    // while vibration is under the threshold of triggering a forceful sensation
+    // fade it out so the previous peek is perceived more smooth and fades out
+    // when under bottom threshold of barely perceiveable vibration, set to 0
+    // to save battery
+    if (lastVibration > 75) {
+      vibration = constrain(lastVibration * 0.75, 0, 255);
+    } else {
+      vibration = 0;
+    }
+    analogWrite(vibrationPin, vibration);
+  }
+
+  // store this vibration impulse for possible fading out in next loop
+  lastVibration = vibration;
+
+
+
+
+  // interval at which to send and receive from bluetooth, 24fps
+  delay(1000 / 24);
+
+  noTone(buzzerPin);
+
+  lastHeading = heading;
+  lastPitch = pitch;
+  lastRoll = roll;
 }
 
 
@@ -243,8 +327,6 @@ void setRGB (int led, int red, int green, int blue)
   red = map(red, 0, 255, 4096, 0);
   green = map(green, 0, 255, 4096, 0);
   blue = map(blue, 0, 255, 4096, 0);
-
-  Serial.println("setRGB " +  led);
 
   // store channels for each color
   switch (led) {
