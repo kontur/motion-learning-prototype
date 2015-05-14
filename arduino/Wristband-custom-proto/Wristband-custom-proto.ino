5bd790965c4ae2bcf82da909eab054ea28a8e3fa
Johannes Neumeier <hello@johannesneumeier.com>
2015-05-14 12:58:44 +0300

    first step and try outs with new color adjusting logic

--- arduino/Wristband-custom-proto/Wristband-custom-proto.ino
+++ arduino/Wristband-custom-proto/Wristband-custom-proto.ino
@@ -43,6 +43,11 @@ SoftwareSerial mySerial(rx, tx);
 int data[2];
 int serialIndex = 0;
 
+// desired fps rate between loops
+// NOTE that this is in reality MUCH lower, ~4 fps
+float fps = 12;
+float msPerFrame = 1000 / fps; // ~83
+  
 int lastR = 0;
 int lastG = 100;
 int lastB = 200;
@@ -153,6 +158,9 @@ float lastBrightness = 1;
 
 int rgbColor[3];
 int lastVibration = 0;
+// factor by which to reduce vibration values in cases where the new value is lower than the previous
+// this speeds up "shuting" down the vibration to feel more responsive
+float vibrationDecay = 0.75; 
 
 int note = 0;
 int lastNote = 0;
@@ -171,22 +179,26 @@ RGBConverter col = RGBConverter();
 
 // in loop variables
 // declared outside the loop and reused to save initialization every loop
+
+// arrays holding the sensor readings
 float accel[3];
 float gyro[3];
 float mag[3];
 
+// variables holding the "calculated" IMU values (note, orientation is a helper to get pitch and roll)
 float heading;
 float orientation[2];
 float pitch;
 float roll;
 
+// various combined changes in acceleration and rotation
 float combinedAccelerationChange;
 float absCombinedAccelerationChange;
 float combinedRotationChange;
 float absCombinedRotationChange;
 float combinedChange;
 
-float threshold = 5.0;
+float threshold = 2.5;
 String json;
 
 void loop ()
@@ -225,12 +237,6 @@ void loop ()
   }
   */
 
-  /*
-  lastHeading = heading;
-  lastPitch = pitch;
-  lastRoll = roll;
-  */
-
 
 
   getAccel(&accel[0]);
@@ -245,6 +251,26 @@ void loop ()
   );
   pitch = orientation[0];
   roll = orientation[1];
+
+  /*
+  // ease the readings for pitch, roll and heading to make them more smooth
+  heading = lastHeading * easing + (1 - heading * easing);
+  pitch = lastPitch * easing + (1 - pitch * easing);
+  roll = lastRoll * easing + (1 - roll * easing);
+  */
+  
+  // store current values for next round to compare
+  lastHeading = heading;
+  lastPitch = pitch;
+  lastRoll = roll;
+  
+  Serial.print("heading ");
+  Serial.println(heading);
+  Serial.print("pitch ");
+  Serial.println(pitch);
+  Serial.print("roll ");
+  Serial.println(roll);
+  
 
   for (int i = 0; i < 3; i++) {
     // ease the new values to contain a portion of the previous value, thus making them less
@@ -322,6 +348,7 @@ void loop ()
   Serial.println("---");
   */
 
+  /*
   Serial.println("Combined percentual acceleration change: ");
   Serial.println(combinedAccelerationChange);
   //Serial.println(absCombinedAccelerationChange);
@@ -332,35 +359,35 @@ void loop ()
 
   Serial.println("Combined percentual change: ");
   Serial.println(combinedChange);
-
+  */
 
   // provide sensory feedback
 
 
 
   if (combinedChange > threshold) {
+    
+    /*
+    // if lastVibrationStart has never occured yet
     if (lastVibrationStart <= 0) {
       lastVibrationStart = now;
     }
 
     int msSinceVibrationStart = now - lastVibrationStart;
-    Serial.print("msSinceVibrationStart: ");
-    Serial.println(msSinceVibrationStart);
+    */
+    int vibration = map(combinedChange, threshold, 200.0, 100, 255);
 
-    //if (msSinceVibrationStart > 500) {
-      Serial.println("STAAART");
-      int vibration = map(combinedChange, threshold, 200.0, 110, 255);
-
-      Serial.print("vibration ");
-      Serial.println(vibration);
-      analogWrite(vibrationPin, vibration);
-      lastVibrationStart = now;
-    //}
     /*
-    if (msSinceVibrationStart > 50 && msSinceVibrationStart < 500) {
-      Serial.println("STOOOP");
-      analogWrite(vibrationPin, 0);
+    if (vibration <= lastVibration + 15) {
+      vibration *= vibrationDecay;
     }
+    */
+    Serial.print("vibration ");
+    Serial.println(vibration);
+    analogWrite(vibrationPin, vibration);
+    /*
+    lastVibrationStart = now;
+    lastVibration = vibration;
     */
   } else {
     analogWrite(vibrationPin, 0);
@@ -373,7 +400,35 @@ void loop ()
     analogWrite(vibrationPin, i);
     delay(500);
   }
-  */
+  */  
+  
+  // leds
+  
+  // roll: 90 is all up, -90 all down
+  // pitch: 90 all left, 90 all right
+  
+  // the more the roll is away from 0, the less pitch should account for
+  float rollFactor = map(roll, -90.0, 90.0, 0, 100);
+  float pitchFactor = map(pitch, -90.0, 90.0, 0, 100);
+  
+  rollFactor = rollFactor / 100;
+  pitchFactor = pitchFactor / 100;
+  
+  Serial.println(rollFactor);
+  Serial.println(pitchFactor);
+  
+  float hue = pitchFactor * rollFactor + rollFactor * (1 - rollFactor);
+  Serial.print("hue: ");
+  Serial.println(hue);
+  
+  H2R_HSBtoRGBfloat(hue, 1, 1, &rgbColor[0]);
+  setRGBs(rgbColor[0], rgbColor[1], rgbColor[2]);
+  Serial.print(rgbColor[0]);
+  Serial.print(", ");
+  Serial.print(rgbColor[1]);
+  Serial.print(", ");
+  Serial.println(rgbColor[2]);
+
 
 
   String json = "{ heading: " + String(heading) +
@@ -395,12 +450,13 @@ void loop ()
   mySerial.println(json);
   //Serial.println(json);
 
-  float fps = 1000 / 12; // ~83
   Serial.print("Actual framerate: ");
-  Serial.println(1000 / (now - lastFrame));
+  float actualFps = 1000 / (now - lastFrame);
+  Serial.println(actualFps);
 
-  // compensate to achieve a delay as close as possible to the actual desired framerate
-  float delayUntilNextFrame = min(max(0, fps - ((now - lastFrame) - fps)), fps);
+  // compensate to achieve a delay between frames as close as possible to the actual desired framerate
+  float delayUntilNextFrame = min(max(0, msPerFrame - ((now - lastFrame) - msPerFrame)), msPerFrame);
+  Serial.print("Delay until next frame: ");
   Serial.println(delayUntilNextFrame);
 
   lastFrame = now;
